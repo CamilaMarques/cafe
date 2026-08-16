53446f09dd6b48b41917703d36bf962603db9b5b
Camila Marques <camilamarquesa26@gmail.com>
2026-08-15 21:16:58 -0300

    feat: dtops e entidades

--- src/main/java/com/aromaorigem/aromaorigem/dto/CadastroRequest.java
+++ src/main/java/com/aromaorigem/aromaorigem/dto/CadastroRequest.java
@@ -1,4 +1,13 @@
 package com.aromaorigem.aromaorigem.dto;
 
-public record CadastroRequest(String nome, String email, String senha) {
-}
+public record CadastroRequest(
+        String nome,
+        String email,
+        String senha,
+        String cep,
+        String rua,
+        String numero,
+        String cidade,
+        String estado,
+        String complemento
+) {}
